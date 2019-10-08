2bb369074767933ee8ee262d23b3c027c80288a5
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2019-10-08 17:02:02 +0300

    Added comments

--- main.py
+++ main.py
@@ -1,128 +1,126 @@
 import math
 import os
-import time
+import shutil
 
 
-def compress(bytes_to_compress):
+# Helper class for reading the file in formatted way
+class FileReader:
+    __path = ''
+    __file = None
+    __byte = None
+
+    def __init__(self, path):
+        self.__path = path
+        self.__file = open(path, 'rb')
+        self.__byte = self.__file.read(1)
+
+    # returns next byte of the file
+    def get_next(self):
+        result = self.__byte
+        self.__byte = self.__file.read(1)
+        return result
+
+    # returns true if there are byte to read
+    def has_bytes(self):
+        return self.__byte
+
+    # closes the stream
+    def close(self):
+        self.__file.close()
+
+
+# reads 'from_file' and writes compressed version to 'to_file'
+def compress(from_file, to_file):
+    file_reader = FileReader(from_file)
+
+    # maps substring to n
     d = {b'': 0}
     s = b''
-    t = []
 
-    result = b''
+    byte = None
 
-    ti = time.time()
+    with open(to_file, 'wb') as sw:
+        while file_reader.has_bytes():
+            byte = file_reader.get_next()
+            sc = s + byte
+            if sc in d:
+                s = sc
+            else:
+                sw.write(convert_to_binary(d[s], byte))
+                d[sc] = len(d)
+                s = b''
 
-    for byte in bytes_to_compress:
-        byte = int.to_bytes(byte, length=1, byteorder='big', signed=False)
-        sc = s + byte
-        if sc in d:
-            s = sc
-        else:
-            t.append((d[s], byte))
-            d[sc] = len(d)
-            s = b''
-
-    print(time.time() - ti)
-    ti = time.time()
-
-    if len(s) > 0:
-        t.append(t[d[s] - 1])
-
-    for k, v in t:
-        result += convert_to_binary(k, v)
-
-    print(time.time() - ti)
-    return result
+        # is there are overhead which is already exist in d
+        if len(s) > 0:
+            sw.write(convert_to_binary(d[s], byte))
 
 
-def decompress(bytes):
+# reads 'from_file' and writes decompressed version to 'to_file'
+def decompress(from_file, to_file):
+    file_reader = FileReader(from_file)
+
     d = {}
-
-    result = b''
-
-    i = 0
     value = 0
     n = 1
 
-    while i < len(bytes):
-        byte = bytes[i]
-        value *= 2 ** 7
-        value += byte % 128
-        if byte >= 128:
-            i += 1
+    with open(to_file, 'wb') as sw:
+        while file_reader.has_bytes():
+            byte = int.from_bytes(bytes=file_reader.get_next(), signed=False, byteorder='big')
+            value *= 2 ** 7
+            value += byte % 128
 
-            x = b''
-            x += int.to_bytes(bytes[i], length=1, signed=False, byteorder='big')
+            # if last byte
+            if byte >= 128:
+                x = b''
+                # adding symbol byte
+                x += file_reader.get_next()
 
-            if value in d.keys():
-                x = d[value] + x
+                if value in d.keys():
+                    x = d[value] + x
 
-            d[n] = x
+                d[n] = x
 
-            result += x
-            value = 0
-            n += 1
-        i += 1
-
-    return result
+                sw.write(x)
+                value = 0
+                n += 1
 
 
+# returns minimum amount of bits to represent given integer
 def get_length_in_bits(integer):
     if integer <= 1:
         return 1
     return int(math.log2(integer)) + 1
 
 
+# converts pair to required binary representation
 def convert_to_binary(value, x):
     length = get_length_in_bits(value)
     number_of_bytes = math.ceil(length / 7)
-    result = 0
+    bytes = bytearray(number_of_bytes)
 
-    degree = 1
-
-    for i in range(number_of_bytes):
-        result += (value % 128) * degree
+    for i in range(number_of_bytes - 1, -1, -1):
+        bytes[i] = value % 128
         value //= 128
-        degree *= 128
-        if i == 0:
-            result += degree
-        degree *= 2
 
-    result *= 256
-    result = result ^ int.from_bytes(bytes=x, byteorder='big', signed=False)
-    return int.to_bytes(result, length=number_of_bytes + 1, byteorder='big', signed=False)
+    # set last byte indicator
+    bytes[number_of_bytes - 1] = bytes[number_of_bytes - 1] | 128
+    return bytes + x
 
 
-def read_file(path):
-    file = b''
-    with open(path, 'rb') as sr:
-        byte = sr.read()
-        while byte:
-            file += byte
-            byte = sr.read()
-    return file
-
-
-def write_file(path, array):
-    with open(path, 'wb') as sw:
-        sw.write(array)
-
-
-def compress_to_file(from_file, to_file):
-    input_file = read_file(from_file)
-    compressed = compress(input_file)
-    write_file(to_file, compressed)
-
-
-def decompress_to_file(from_file, to_file):
-    input_file = read_file(from_file)
-    decompressed = decompress(input_file)
-    write_file(to_file, decompressed)
+# adds suffix to file name
+# {a}.{b} -> {a}{suffix}.{b}
+def add_suffix(file_name, suffix):
+    parts = file_name.split('.')
+    return parts[0] + suffix + '.' + parts[1]
 
 
 def main():
-    # compress_to_file(from_file='./dataset/exe/file1.exe',
-    #                 to_file='./IlshatFatkhullinOutputs/dataset/exe/file1.exe')
+    # delete folder if exists
+
+    if 'IlshatFatkhullinOutputs' in os.listdir('./'):
+        shutil.rmtree('./IlshatFatkhullinOutputs/')
+
+    # create directory structure
 
     os.mkdir('./IlshatFatkhullinOutputs/')
     os.mkdir('./IlshatFatkhullinOutputs/dataset')
@@ -135,13 +133,17 @@ def main():
 
         for root, dirs, files in os.walk('./dataset/' + file_type_dir):
             for file in files:
-                compress_to_file(from_file='./dataset/%s/%s' % (file_type_dir, file),
-                                 to_file='./IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, file))
+                from_file = './dataset/%s/%s' % (file_type_dir, file)
+                to_file = './IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, add_suffix(file, 'Compressed'))
+
+                compress(from_file=from_file, to_file=to_file)
 
                 print('%s compressed' % file)
 
-                decompress_to_file(from_file='./IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, file),
-                                   to_file='./IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, 'decompressed_' + file))
+                from_file = './IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, add_suffix(file, 'Compressed'))
+                to_file = './IlshatFatkhullinOutputs/dataset/%s/%s' % (file_type_dir, add_suffix(file, 'Decompressed'))
+
+                decompress(from_file=from_file, to_file=to_file)
 
                 print('%s decompressed' % file)
 
